6b2232fe1ad0edf4dbdd56dbb51f3d581fc0a5d8
Junho Gong <hijunho0811@gmail.com>
2024-02-13 11:07:31 +0900

    deleted original portfolio, migrating to react

---
+++ src/content_option.js
@@ -0,0 +1,46 @@
+const logotext = "Junho";
+const meta = {
+    title: "Junho Gong",
+    description: "Junho Gong, CSE Student. Currently based in Seoul, South Korea",
+};
+
+const introdata = {
+    title: "안녕하세요, 공준호입니다.",
+    animated: {
+        first: "재밌고 쓸모없는 것들을 좋아합니다.",
+        second: "새로운 기술과 창의적인 아이디어로",
+        third: "즐거움을 주는 프로그램을 만듭니다.",
+    },
+    description: "Backend 를 깊이 있게 공부하고 있습니다. 특정 분야에 국한되지 않고, Frontend 에도 관심을 가지는 등 관심사를 넓혀가고 있습니다. ",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/hijunho0811",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/junho-gong-267757236/",
+    twitter: "https://twitter.com",
+};
+export {
+    logotext,
+    meta,
+    introdata,
+    dataabout,
+    contactConfig,
+    socialprofils,
+};
